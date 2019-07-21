c618645ff0ef4ea12ec51df07f0516d89ec7ba83
Mohit Chauhan <mohitchauhan@Mohitcs-MacBook-Pro.local>
2019-07-21 11:34:00 +0530

    Added build src module for dependencies

---
+++ buildSrc/src/main/java/app/weather/buildsrc/dependencies.kt
@@ -0,0 +1,77 @@
+package app.weather.buildsrc
+
+object Versions {
+    const val ktlint = "0.33.0"
+}
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:3.5.0-rc01"
+
+    const val timber = "com.jakewharton.timber:timber:4.7.1"
+
+    const val junit = "junit:junit:4.12"
+    const val robolectric = "org.robolectric:robolectric:4.3"
+    const val mockK = "io.mockk:mockk:1.9.3"
+
+
+    object Kotlin {
+        private const val version = "1.3.41"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+    }
+
+    object Coroutines {
+        private const val version = "1.2.1"
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+        const val rx2 = "org.jetbrains.kotlinx:kotlinx-coroutines-rx2:$version"
+        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+    }
+
+    object AndroidX {
+        const val appcompat = "androidx.appcompat:appcompat:1.1.0-beta01"
+        const val recyclerview = "androidx.recyclerview:recyclerview:1.1.0-alpha06"
+
+        object Fragment {
+            private const val version = "1.1.0-beta01"
+            const val fragment = "androidx.fragment:fragment:$version"
+            const val fragmentKtx = "androidx.fragment:fragment-ktx:$version"
+        }
+
+        const val constraintlayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+
+        const val coreKtx = "androidx.core:core-ktx:1.0.1"
+
+    }
+
+    object RxJava {
+        const val rxJava = "io.reactivex.rxjava2:rxjava:2.2.10"
+        const val rxKotlin = "io.reactivex.rxjava2:rxkotlin:2.3.0"
+        const val rxAndroid = "io.reactivex.rxjava2:rxandroid:2.1.1"
+    }
+
+    object Dagger {
+        private const val version = "2.23.2"
+        const val dagger = "com.google.dagger:dagger:$version"
+        const val androidSupport = "com.google.dagger:dagger-android-support:$version"
+        const val compiler = "com.google.dagger:dagger-compiler:$version"
+        const val androidProcessor = "com.google.dagger:dagger-android-processor:$version"
+    }
+
+
+    object Retrofit {
+        private const val version = "2.6.0"
+        const val retrofit = "com.squareup.retrofit2:retrofit:$version"
+        const val retrofit_rxjava_adapter = "com.squareup.retrofit2:adapter-rxjava2:$version"
+        const val gsonConverter = "com.squareup.retrofit2:converter-gson:$version"
+    }
+
+    object OkHttp {
+        private const val version = "4.0.0"
+        const val okhttp = "com.squareup.okhttp3:okhttp:$version"
+        const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$version"
+    }
+
+
+}
